e03e749e65db4ede7173d5a69be046bd0a892d95
neumond <knifeslaughter@gmail.com>
2014-09-29 03:42:03 +0400

    cleanup, start game with probs, but fails asserts

--- game.py
+++ game.py
@@ -3,10 +3,12 @@ from math import log
 from os.path import getmtime
 from numprint import print_state
 from itertools import product
+from copy import deepcopy
 
 
 size = 4
 KOEF = 0.4
+epsilon = 0.001
 
 
 def get_val(s, x, y):
@@ -208,11 +210,13 @@ def merge_cells(base, add):
     add_result = {}
     base_super_range = sum(map(lambda x: x[1], base.items()))
     add_super_range = sum(map(lambda x: x[1], add.items()))
+    assert abs(base_super_range - 1.) < epsilon
+    assert abs(add_super_range - 1.) < epsilon
 
     # remove space
     s = _d_getzero(base)
     add_zero = _d_getzero(add) - s
-    if add_zero < 0.:
+    if add_zero < -epsilon:
         raise Exception('add_zero < 0 ({}), probably bad input, must be valid Flow result'.format(add_zero))
     _d_add(base_result, (0, -1), s)
     _d_add(add_result, (0, -1), s)
@@ -229,7 +233,7 @@ def merge_cells(base, add):
             continue
         item_apply = []
         for add_item, base_item in product(add_items, base_items):
-            intersect = (base_item[1]) * (add_item[1] / add_range) * (1. / base_super_range)
+            intersect = (base_item[1]) * (add_item[1] / add_range)  # * (1. / base_super_range)
             if add_item[0][0] == base_item[0][0] and add_item[0][1] > base_item[0][1]:
                 # equal numbers, can merge
                 _d_add(base_result, add_item[0][0] * 2, intersect)
@@ -244,7 +248,7 @@ def merge_cells(base, add):
                 item_apply.append((add_item, intersect))
         for item, intersect in item_apply:
             item[1] -= intersect
-            if item[1] < -0.0001:
+            if item[1] < -epsilon:
                 raise Exception('After applying intersect item became negative: - {} = {}'.format(intersect, item[1]))
         add_items = filter_items(add_items)
         base_items = filter_items(base_items)
@@ -271,7 +275,7 @@ def shift_prob_line(row, toidx, levels):
             items_range = sum(map(lambda x: x[1], items))  # size of next level of pyramid
             if items_range < levK:  # level falls completely
                 zero_range = levK - items_range
-                assert zero_range <= _d_getzero(src)
+                assert zero_range <= _d_getzero(src) + epsilon
             #print('items_range={} zero_range={}'.format(items_range, zero_range))
             workedK = 0.
             if items_range + zero_range > 0.:
@@ -286,7 +290,8 @@ def shift_prob_line(row, toidx, levels):
             if workedK < levK:
                 intersect = levK - workedK
                 #print('inter {}: {}'.format(0, intersect))
-                assert intersect <= zero_range
+                #print(intersect, zero_range)
+                assert intersect <= zero_range + epsilon
                 _d_add(target, 0, intersect)
                 _d_add(src, 0, -intersect)
                 _d_add(next_levels, -1, intersect)
@@ -302,12 +307,13 @@ def shift_prob_line(row, toidx, levels):
 
         levels = next_levels
         #print('levelstats', sm, sum(levels.values()), levels)
-        assert sm == sum(levels.values())
+        assert abs(sm - sum(levels.values())) < epsilon
     _d_add(row[size - 1], 0, sm)
     #print(row)
 
 
 def merge_prob_line(row):
+    #print(row)
     for cur_idx in range(len(row) - 1):
         nxt_idx = cur_idx + 1
         cur, nxt = row[cur_idx], row[nxt_idx]
@@ -333,217 +339,43 @@ def merge_prob_line(row):
             del cur[k]
 
 
-#def merge_prob_line(row):
-    #for cur_idx in range(len(row) - 1):
-        #nxt_idx = cur_idx + 1
-        #cur, nxt = row[cur_idx], row[nxt_idx]
-        #print('Base #{} {}'.format(cur_idx, cur))
-        #print('         {}'.format(nxt))
-        #total_shift_prob = 0.
-        #total_sum = _dict_sum(nxt)
-        #levels = _order_sums(nxt)
-        #shift_levels = {}
-        #for nkey, cur_prob in sorted(cur.copy().items(), key=lambda x: -1 if x[0] == 0 else x[0][1]):
-            #if not isinstance(nkey, tuple) or cur_prob == 0.:
-                #continue
-            #number, level = nkey
-            #print(' +checking {}'.format(nkey))
-            #n2 = number * 2
-            #affected_nxt_items = _merge_affected(nxt, number, level)
-            #print('  affected={}'.format(affected_nxt_items))
-            #lvlsum = levels[level] if level < len(levels) else nxt.get(0, 0.)
-
-            #nxt_prob = sum(map(lambda x: x[1], affected_nxt_items))
-            #print('  nxt_prob={}/{}'.format(nxt_prob, lvlsum))
-            #if nxt_prob == 0.:
-                #continue
-
-            #nxt_prob /= lvlsum  # expand narrowed range to 0..1
-            #merge_prob = cur_prob * nxt_prob
-
-            #print('  num={}, merge={}'.format(number, merge_prob))
-            #total_shift_prob += merge_prob
-
-            #cur[nkey] = cur_prob - merge_prob
-            #cur[n2] = cur.get(n2, 0.) + merge_prob
-            #for k, v in affected_nxt_items:
-                #nxt[k] = nxt[k] - merge_prob * (nxt[k] / nxt_prob)
-            #nxt[0] = nxt.get(0, 0.) + merge_prob
-
-            ##_affected_levels
-        #print('  After merge: {}'.format(cur))
-        #if total_shift_prob != 0. and False:
-            #print('Shift at #{} with k={}'.format(nxt_idx, total_shift_prob))
-            #print('  before shift {}'.format(row))
-            #for shift_idx in range(nxt_idx + 1, len(row)):
-                ## a = b * prob
-                ## b = b * (1 - prob)
-                #a, b = row[shift_idx - 1], row[shift_idx]
-                ##assert sum(list(a.values())) == 1.
-                ##assert sum(list(b.values())) == 1.
-                #k = total_shift_prob / total_sum
-                #a = _add_dict(a, _mult_dict(b, k))
-                #b = _mult_dict(b, 1 - k)
-                #row[shift_idx - 1] = a
-                #row[shift_idx] = b
-            ##row[-1][0] = row[-1].get(0, 0.) + total_shift_prob
-            #print('  after shift  {}'.format(row))
-    ## flatten dicts
-    #for cur in row:
-        #for k, v in cur.copy().items():
-            #if not isinstance(k, tuple):
-                #continue
-            #number = k[0]
-            #cur[number] = cur.get(number, 0.) + v
-            #del cur[k]
-
-
 def check_and_clean_row(row):
     for cur in row:
         s = sum(cur.values())
-        if abs(s - 1.) > 0.001:
+        if abs(s - 1.) > epsilon:
             raise Exception('Invalid prob sum {} for {}'.format(s, cur))
         for k, v in list(cur.items()):
-            if not (0. <= v <= 1.):
+            if not (-epsilon <= v <= (1. + epsilon)):
                 raise Exception('Invalid value {} for key {} in {}'.format(v, k, cur))
+            if v < 0.:
+                v = 0.
+            if v > 1.:
+                v = 1.
             if v == 0.:
                 del cur[k]
 
-
 # dir: 0 - up, 1 - right, 2 - down, 3 - left
 
-#def probmove(s, direct):
-    #backward = direct == 1 or direct == 2
-    #vert = direct == 0 or direct == 2
+def probmove(s, direct):
+    backward = direct == 1 or direct == 2
+    vert = direct == 0 or direct == 2
 
-    #r2step = -1 if backward else 1
-    #r2args = (size - 1, -1, -1) if backward else (0, size, 1)
-    #r2args_1 = (size - 1, 0, -1) if backward else (0, size - 1, 1)
-    #def r2args_sub(coord):
-        #return (coord + 1, size, 1) if backward else (coord - 1, -1, -1)
+    r2step = -1 if backward else 1
+    r2args = (size - 1, -1, -1) if backward else (0, size, 1)
 
-    ## store here merged probabilities to exclude cumulative effects
-    ## i.e. merge can be done once per move
-    #pending_merges = []
-
-    ## merge step
-    #for c1 in range(size):
-        #for c2 in range(*r2args_1):
-            #x, y = (c1, c2) if vert else (c2, c1)
-            #xc, yc = (c1, c2 + r2step) if vert else (c2 + r2step, c1)
-            #v = get_val(s, x, y)
-            #vnext = get_val(s, xc, yc)
-            #total_shift_prob = 0.
-            #for number, prob in v.items():
-                #if number == 0 or number not in vnext:
-                    #continue
-                #merge_prob = prob * vnext[number]
-                #if merge_prob == 0.:
-                    #continue
-                #total_shift_prob += merge_prob
-                ## TODO: merge
-
-            #if total_shift_prob != 0.:
-                #for c22 in range(*r2args_sub_1(c2)):
-
-                #prev = v
-                ##print('enum for ({}, {}), #{}: '.format(x, y, number), end='')
-                #for c22 in range(*r2args_sub(c2)):
-                    #xc, yc = (c1, c22) if vert else (c22, c1)
-                    ##print('({}, {}) '.format(xc, yc), end='')
-                    #vv = get_val(s, xc, yc)
-
-                    #flow = amount * vv.get(0, 0.)
-                    #if flow != 0.:
-                        #vv[number] = vv.get(number, 0.) + flow
-                        #vv[0] = vv.get(0, 0.) - flow
-                        #prev[number] = prev.get(number, 0.) - flow
-                        #prev[0] = prev.get(0, 0.) + flow
-
-                    #amount = flow
-                    #prev = vv
-                ##print()
-
-    #for x, y, number, prob in pending_merges:
-        #v = get_val(s, x, y)
-        #v[number] = v.get(number, 0.) + prob
-
-    #for v in s:
-        #if abs(sum(list(v.values())) - 1.0) > 0.001:
-            #raise Exception('Sum of probabilities is not 1 ({}): {}'.format(sum(list(v.values())), v))
-        #for k in list(v.keys()):
-            #if v[k] <= 0.:
-                #del v[k]
-
-
-def probtest(row):
-    for c2 in range(size):
-        v = row[c2]
-        if v == 0:
-            continue
-        for c22 in range(c2 - 1, -1, -1):
-            prev = row[c22 + 1]
-            vv = row[c22]
-
-            if isinstance(vv, int) and vv == prev:
-                # merge
-                row[c22] = (vv * 2,)
-                row[c22 + 1] = 0
-            elif vv == 0:
-                # move
-                row[c22] = prev
-                row[c22 + 1] = 0
-    for c2 in range(size):
-        if type(row[c2]) == tuple:
-            row[c2] = row[c2][0]
-
-
-def probtest2(row):
-    for c2 in range(size):
-        v = row[c2]
-        if v == 0:
-            continue
-        for c22 in range(c2 - 1, -1, -1):
-            prev = row[c22 + 1]
-            vv = row[c22]
-            if vv == 0:
-                # move
-                row[c22] = prev
-                row[c22 + 1] = 0
-    for c2 in range(size - 1):
-        v = row[c2]
-        v1 = row[c2 + 1]
-        if v == v1:
-            row[c2] = v * 2
-            row.pop(c2 + 1)
-            row.append(0)
-    for c2 in range(size):
-        if type(row[c2]) == tuple:
-            row[c2] = row[c2][0]
-
-
-def probstress():
-    import random
-    p = {}
-    for i in range(100000):
+    for c1 in range(size):
         row = []
-        for k in range(size):
-            row.append(4 if random.random() < 0.5 else 0)
-        a, b = row.copy(), row.copy()
-        probtest(a)
-        probtest2(b)
-        assert a == b
-        row = tuple(a)
-        p[row] = p.get(row, 0) + 1
-    #p = {k: v / s for k, v in p.items()}
-    res = [{}, {}, {}, {}]
-    for k, v in p.items():
-        for i in range(size):
-            res[i][k[i]] = res[i].get(k[i], 0) + v
-    for i in range(size):
-        s = sum(res[i].values())
-        res[i] = {k: v / s for k, v in res[i].items()}
-    return res
+        for c2 in range(*r2args):
+            x, y = (c1, c2) if vert else (c2, c1)
+            row.append(get_val(s, x, y))
+        check_and_clean_row(row)
+        flow_prob_line(row)
+        check_and_clean_row(row)
+        merge_prob_line(row)
+        check_and_clean_row(row)
+        for i, c2 in enumerate(range(*r2args)):
+            x, y = (c1, c2) if vert else (c2, c1)
+            set_val(s, x, y, row[i])
 
 
 def pathcells(s, direct):
@@ -570,6 +402,9 @@ def emptycells(s):
     return len(list(filter(lambda x: x == 0, s)))
 
 
+prob_4 = 1/3  # TODO: not actually proper, original game have 10%
+
+
 def put_random(s):
     emptycount = len(list(filter(lambda x: x == 0, s)))
     if emptycount == 0:
@@ -581,9 +416,27 @@ def put_random(s):
         if i > 0:
             i -= 1
             continue
-        s[j] = random.choice([2, 2, 4])
+        if random.random() < prob_4:
+            s[j] = 4
+        else:
+            s[j] = 2
         return True
     raise Exception('Random cell not found')
+
+
+def put_random_prob(s):
+    emptycount = sum(v[0] if 0 in v else 0. for v in s)
+    if emptycount == 0:
+        return False
+    for v in s:
+        if 0 not in v:
+            continue
+        inter2 = (1 - prob_4) * v[0] / emptycount
+        inter4 = prob_4 * v[0] / emptycount
+        _d_add(v, 2, inter2)
+        _d_add(v, 4, inter4)
+        _d_add(v, 0, -(inter2 + inter4))
+    return True
 
 
 def move_tree(state, backtrack):
@@ -602,13 +455,33 @@ def move_tree(state, backtrack):
     return maxfldsum
 
 
+def make_prob_state(s):
+    return [{v: 1.} for v in s]
+
+
+def move_tree2(state, backtrack):
+    if backtrack <= 0:
+        return sum(_d_getzero(v) for v in state)  # best is most empty
+    state = deepcopy(state)
+    put_random_prob(state)
+    x = []
+    for direct in range(4):
+        s = deepcopy(state)
+        #print(s)
+        probmove(s, direct)
+        x.append(move_tree2(s, backtrack - 1))
+    return min(x)
+
+
 def choose_move(state, backtrack):
     bestmove, bestmove_dir = None, None
     for direct in range(4):
         s = state.copy()
         mvs, flds, fldsum, failsum = move(s, direct)
-        fldsum += move_tree(s, backtrack)
-        if mvs > 0 and (bestmove is None or fldsum > bestmove):
+        if mvs <= 0:
+            continue
+        fldsum = move_tree2(make_prob_state(s), backtrack)
+        if bestmove is None or fldsum > bestmove:
             bestmove, bestmove_dir = fldsum, direct
     #print('bestmove={}'.format(bestmove))
     return bestmove_dir
@@ -631,7 +504,7 @@ def main():
             backtrack += 1
         if max(s) >= 1024:
             backtrack += 1
-        backtrack = 3
+        backtrack = 2
         bestmove_dir = choose_move(s, backtrack)
         rnd = False
         if bestmove_dir is not None:
@@ -647,8 +520,8 @@ def main():
     print('GAME OVER')
     print('Maximum is {}'.format(max(s)))
 
-    with open('top.txt', 'a') as f:
-        f.write('{}: {}\n'.format(vers, max(s)))
+    #with open('top.txt', 'a') as f:
+        #f.write('{}: {}\n'.format(vers, max(s)))
 
 
 if __name__ == '__main__':
